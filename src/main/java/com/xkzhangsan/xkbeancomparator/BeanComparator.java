120636d3b696081a1e0155b336ff3b6da57a5909
xkzhangsan <xkzhangsan@163.com>
2019-10-20 13:35:29 +0800

    优化

--- src/main/java/com/xkzhangsan/xkbeancomparator/BeanComparator.java
+++ src/main/java/com/xkzhangsan/xkbeancomparator/BeanComparator.java
@@ -5,6 +5,8 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
@@ -13,30 +15,23 @@ import org.apache.commons.lang3.ArrayUtils;
 public class BeanComparator {
 
 	/**
-	 * 对比bean属性,默认使用StringBuilder处理
+	 * 对比bean属性 
+	 * Contrast bean properties with StringBuilder processing by
+	 * default
 	 * 
 	 * @param source
 	 * @param target
 	 * @return
 	 */
 	public static String compareBean(Object source, Object target) {
-		return compareBean(source, target, null, false);
+		return compareBean(source, target, null);
 	}
 
 	/**
-	 * 对比bean属性，支持选择StringBuffer or StringBuilder
-	 * 
-	 * @param source
-	 * @param target
-	 * @param isochronous use StringBuffer or StringBuilder
-	 * @return
-	 */
-	public static String compareBean(Object source, Object target, boolean isochronous) {
-		return compareBean(source, target, null, isochronous);
-	}
-
-	/**
-	 * 对比bean属性，支持属性名称map，只对比map中包含的属性值
+	 * 对比bean属性，支持属性名称map，只对比map中包含的属性值 
+	 * Compare the bean properties, support the
+	 * property name map, and only compare the property values contained in the
+	 * map
 	 * 
 	 * @param source
 	 * @param target
@@ -44,20 +39,6 @@ public class BeanComparator {
 	 * @return
 	 */
 	public static String compareBean(Object source, Object target, Map<String, String> propertyTranslationMap) {
-		return compareBean(source, target, propertyTranslationMap, false);
-	}
-
-	/**
-	 * 对比bean属性,支持所有参数
-	 * 
-	 * @param source
-	 * @param target
-	 * @param propertyTranslationMap
-	 * @param isochronous  use StringBuffer or StringBuilder
-	 * @return
-	 */
-	public static String compareBean(Object source, Object target, Map<String, String> propertyTranslationMap,
-			boolean isochronous) {
 		if (source == null) {
 			throw new RuntimeException("source is null.");
 		}
@@ -78,11 +59,13 @@ public class BeanComparator {
 				throw new RuntimeException("source.class has complex property.");
 			}
 		}
-		return compareProperty(source, target, clazz, fields, propertyTranslationMap, isochronous);
+		return compareProperty(source, target, clazz, fields, propertyTranslationMap);
 	}
 
 	/**
-	 * 根据参数isochronous选择StringBuffer or StringBuilder，进行属性对比
+	 * 比较所有属性值，根据传参转换属性名称，组装结果（核心逻辑） 
+	 * Compare all attribute values, transform
+	 * attribute names based on parameters, assemble results (core logic)
 	 * 
 	 * @param source
 	 * @param target
@@ -93,31 +76,8 @@ public class BeanComparator {
 	 * @return
 	 */
 	private static String compareProperty(Object source, Object target, Class<?> clazz, Field[] fields,
-			Map<String, String> propertyTranslationMap, boolean isochronous) {
-		if (isochronous) {
-			StringBuffer sBuffer = new StringBuffer();
-			compareUnit(source, target, clazz, fields, propertyTranslationMap, sBuffer, null);
-			return sBuffer.toString();
-		} else {
-			StringBuilder sBuilder = new StringBuilder();
-			compareUnit(source, target, clazz, fields, propertyTranslationMap, null, sBuilder);
-			return sBuilder.toString();
-		}
-	}
-
-	/**
-	 * 比较所有属性值，根据传参转换属性名称，组装结果（核心逻辑）
-	 * 
-	 * @param source
-	 * @param target
-	 * @param clazz
-	 * @param fields
-	 * @param propertyTranslationMap
-	 * @param sBuffer
-	 * @param sBuilder
-	 */
-	private static void compareUnit(Object source, Object target, Class<?> clazz, Field[] fields,
-			Map<String, String> propertyTranslationMap, StringBuffer sBuffer, StringBuilder sBuilder) {
+			Map<String, String> propertyTranslationMap) {
+		StringBuilder sBuilder = new StringBuilder();
 		for (Field field : fields) {
 			String name = field.getName();
 			String nameMapped = null;
@@ -131,15 +91,17 @@ public class BeanComparator {
 			Class<?> propertyClazz = field.getClass();
 			Object sourcePropertyValue = getValueByField(source, clazz, name);
 			Object targetPropertyValue = getValueByField(target, clazz, name);
-			if(nameMapped != null){
+			if (nameMapped != null) {
 				name = nameMapped;
 			}
-			comparePropertyValue(sBuffer, sBuilder, name, propertyClazz, sourcePropertyValue, targetPropertyValue);
+			comparePropertyValue(sBuilder, name, propertyClazz, sourcePropertyValue, targetPropertyValue);
 		}
+		return sBuilder.toString();
 	}
 
 	/**
 	 * 获取属性值
+	 * Get attribute value
 	 * 
 	 * @param obj
 	 * @param clazz
@@ -171,24 +133,37 @@ public class BeanComparator {
 
 	/**
 	 * 对比属性值
+	 * Contrast attribute value
 	 * 
-	 * @param sBuffer
 	 * @param sBuilder
 	 * @param name
 	 * @param propertyClazz
 	 * @param sourcePropertyValue
 	 * @param targetPropertyValue
 	 */
-	private static void comparePropertyValue(StringBuffer sBuffer, StringBuilder sBuilder, String name,
-			Class<?> propertyClazz, Object sourcePropertyValue, Object targetPropertyValue) {
+	private static void comparePropertyValue(StringBuilder sBuilder, String name, Class<?> propertyClazz,
+			Object sourcePropertyValue, Object targetPropertyValue) {
 		if (!Objects.equals(sourcePropertyValue, targetPropertyValue)) {
-			String sourcePropertyValueStr = sourcePropertyValue == null ? "" : sourcePropertyValue.toString();
-			String targetPropertyValueStr = targetPropertyValue == null ? "" : targetPropertyValue.toString();
-			if (sBuffer != null) {
-				sBuffer.append(name + ":" + sourcePropertyValueStr + "->" + targetPropertyValueStr + ",");
-			} else if (sBuilder != null) {
-				sBuilder.append(name + ":" + sourcePropertyValueStr + "->" + targetPropertyValueStr + ",");
-			}
+			String sourcePropertyValueStr = getFormatStr(sourcePropertyValue);
+			String targetPropertyValueStr = getFormatStr(targetPropertyValue);
+			sBuilder.append(name + ":" + sourcePropertyValueStr + "->" + targetPropertyValueStr + ",");
+		}
+	}
+	
+	/**
+	 * 格式化属性
+	 * Format attribute
+	 * @param propertyValue
+	 * @return
+	 */
+	private static String getFormatStr(Object propertyValue){
+		if(propertyValue == null){
+			return "";
+		}
+		if(propertyValue instanceof Date){
+			return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format((Date)propertyValue);
+		}else{
+			return propertyValue.toString();
 		}
 	}
 }
--- src/test/java/com/xkzhangsan/xkbeancomparator/test/BeanTest.java
+++ src/test/java/com/xkzhangsan/xkbeancomparator/test/BeanTest.java
@@ -12,12 +12,11 @@ public class BeanTest {
 	public static void main(String[] args) {
 		test1();
 		test2();
-		test3();
-
 	}
 
 	/**
-	 * 对比所有属性，使用StringBuilder
+	 * 对比所有属性
+	 * Compare all attributes
 	 */
 	public static void test1() {
 		Bean b1 = new Bean();
@@ -44,6 +43,8 @@ public class BeanTest {
 
 	/**
 	 * 只对比map中的属性，并根据key替换属性描述
+	 * Compare only the attributes in the map, 
+	 * and replace the attribute description based on the key
 	 */
 	public static void test2() {
 		Map<String, String> map = new HashMap<>();
@@ -68,32 +69,6 @@ public class BeanTest {
 		b2.setDate(new Date());
 
 		String s = BeanComparator.compareBean(b1, b2, map);
-		System.out.println(s);
-	}
-
-	/**
-	 * 对比所有属性，使用StringBuffer
-	 */
-	public static void test3() {
-		Bean b1 = new Bean();
-		b1.setId(1);
-		b1.setName("aa");
-		b1.setBigD(new BigDecimal("111111111111.12"));
-		b1.setDate(new Date());
-
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		Bean b2 = new Bean();
-		b2.setId(1);
-		b2.setName("aa2");
-		b2.setBigD(new BigDecimal("1111111111111.15"));
-		b2.setDate(new Date());
-
-		String s = BeanComparator.compareBean(b1, b2, true);
 		System.out.println(s);
 	}
 
